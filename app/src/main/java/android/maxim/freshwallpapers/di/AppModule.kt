382a3f81e38980f791c7430c63a45ed9a65181cf
MaximGuljaev <maximuspo4ta@gmail.com>
2023-06-08 00:36:39 +0300

    add di

---
+++ app/src/main/java/android/maxim/freshwallpapers/di/AppModule.kt
@@ -0,0 +1,10 @@
+package android.maxim.freshwallpapers.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+}
