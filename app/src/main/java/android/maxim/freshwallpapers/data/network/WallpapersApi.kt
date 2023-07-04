a2e40d85c13ed4ceb0658b62a642c0200e5dafb0
MaximGuljaev <maximuspo4ta@gmail.com>
2023-07-04 18:35:33 +0300

    refactor api

--- app/src/main/java/android/maxim/freshwallpapers/data/network/WallpapersApi.kt
+++ app/src/main/java/android/maxim/freshwallpapers/data/network/WallpapersApi.kt
@@ -1,26 +1,11 @@
 package android.maxim.freshwallpapers.data.network
 
-import android.maxim.freshwallpapers.data.models.Image
 import android.maxim.freshwallpapers.data.models.ImageList
-import okhttp3.ResponseBody
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WallpapersApi {
-
-    @GET(".")
-    fun getCategoriesList(
-        @Query("key") api_key: String,
-        @Query("category") category: String
-    ): Call<ResponseBody>
-
-    /*@GET(".")
-    fun getImageList(
-        @Query("key") api_key: String,
-        @Query("category") category: String
-    ): Call<ResponseBody>*/
 
     @GET(".")
     suspend fun getImageList(
