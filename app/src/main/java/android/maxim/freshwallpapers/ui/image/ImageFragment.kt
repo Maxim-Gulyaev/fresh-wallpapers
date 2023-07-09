6d8b4cb9f8be21c18e861b26618796d8d0fde1c5
MaximGuljaev <maximuspo4ta@gmail.com>
2023-07-09 22:56:10 +0300

    add image to ImageFragment

--- app/src/main/java/android/maxim/freshwallpapers/ui/image/ImageFragment.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/image/ImageFragment.kt
@@ -1,15 +1,14 @@
 package android.maxim.freshwallpapers.ui.image
 
 import android.maxim.freshwallpapers.R
-import android.maxim.freshwallpapers.ui.collections.CollectionsViewModel
-import android.maxim.freshwallpapers.utils.Constants.TAG
+import android.maxim.freshwallpapers.databinding.FragmentImageBinding
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,13 +16,22 @@ class ImageFragment: Fragment(R.layout.fragment_image) {
 
     private var largeImageURL: String? = null
     private val imageViewModel: ImageViewModel by viewModels()
+    private var _binding: FragmentImageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentImageBinding.inflate(layoutInflater, container, false)
         largeImageURL = arguments?.getString("largeImageURL")
-        return super.onCreateView(inflater, container, savedInstanceState)
+        Picasso.get().load(largeImageURL).into(binding.ivImage)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/android/maxim/freshwallpapers/ui/image/ImageViewModel.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/image/ImageViewModel.kt
@@ -2,7 +2,9 @@ package android.maxim.freshwallpapers.ui.image
 
 import android.maxim.freshwallpapers.data.repository.WallpapersRepository
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
