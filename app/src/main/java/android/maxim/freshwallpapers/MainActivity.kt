051a76d7be6aedb7cda22b284937f00ed3e5e833
MaximGuljaev <maximuspo4ta@gmail.com>
2023-07-27 18:42:22 +0300

    add google analytics

--- app/src/main/java/android/maxim/freshwallpapers/MainActivity.kt
+++ app/src/main/java/android/maxim/freshwallpapers/MainActivity.kt
@@ -2,12 +2,20 @@ package android.maxim.freshwallpapers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        analytics = Firebase.analytics
     }
 }
