30ab2bf0c84bff02b6c7a694e7a788b3fc602297
MaximGuljaev <maximuspo4ta@gmail.com>
2023-07-29 22:45:25 +0300

    add toolbat in settingsfragment

--- app/src/main/java/android/maxim/freshwallpapers/ui/settings/SettingsFragment.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/settings/SettingsFragment.kt
@@ -1,7 +1,33 @@
 package android.maxim.freshwallpapers.ui.settings
 
 import android.maxim.freshwallpapers.R
+import android.maxim.freshwallpapers.databinding.FragmentSettingsBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
