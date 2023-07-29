3a930fc7a00cac713623ae600e4c24d9a5bfdc63
MaximGuljaev <maximuspo4ta@gmail.com>
2023-07-29 16:10:34 +0300

    refactor collections viewModel

--- app/src/main/java/android/maxim/freshwallpapers/ui/collections/CollectionsViewModel.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/collections/CollectionsViewModel.kt
@@ -1,10 +1,6 @@
 package android.maxim.freshwallpapers.ui.collections
 
-import android.maxim.freshwallpapers.data.models.WallpapersCollection
 import android.maxim.freshwallpapers.data.repository.WallpapersRepository
-import android.maxim.freshwallpapers.utils.Constants
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,15 +12,8 @@ class CollectionsViewModel @Inject constructor(): ViewModel() {
     @Inject
     lateinit var repository: WallpapersRepository
 
-
-    /*fun getCategoriesList(): List<String> {
-        Log.d(Constants.TAG, "CollectionsViewModel.getCategoriesList()")
-        return repository.getCollectionsList()
-    }*/
-
-    suspend fun getCategoriesList() = liveData<List<WallpapersCollection>> {
+    suspend fun getCategoriesList() = liveData {
         repository.getCollectionsList().collect { value ->
-            Log.d(Constants.TAG, "collect $value")
             emit(value)
         }
     }
