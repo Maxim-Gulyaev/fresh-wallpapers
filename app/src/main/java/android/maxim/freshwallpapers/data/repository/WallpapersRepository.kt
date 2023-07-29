f9097cda353205b2c0ac9f642fa7deba4016460e
MaximGuljaev <maximuspo4ta@gmail.com>
2023-07-29 15:07:10 +0300

    get data from firestore

--- app/src/main/java/android/maxim/freshwallpapers/data/repository/WallpapersRepository.kt
+++ app/src/main/java/android/maxim/freshwallpapers/data/repository/WallpapersRepository.kt
@@ -11,14 +11,14 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.EntryPoints
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import retrofit2.Response
 import javax.inject.Inject
-
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class WallpapersRepository @Inject constructor(@ApplicationContext context: Context) {
 
@@ -26,45 +26,29 @@ class WallpapersRepository @Inject constructor(@ApplicationContext context: Cont
         context.applicationContext,
         WallpapersRepositoryEntryPoint::class.java)
     private val wallpapersApi = hiltEntryPoint.wallpapersApi()
-    //private var collectionsList = hiltEntryPoint.collectionsList()
 
     private val firestoreDb = Firebase.firestore
     private val firestoreReference = firestoreDb.collection("collections")
 
-    /*fun getCollectionsList(): List<String> {
-        Log.d(TAG, "WallpapersRepository.getCategoriesList()")
-        return collectionsList.collectionsList
-    }*/
-
-    /*fun getCollectionsList(): Flow<List<WallpapersCollection>> = flow<List<WallpapersCollection>> {
-        val wallpapersCollection = WallpapersCollection("one", "someUrl")
-        val wallpapersCollection2 = WallpapersCollection("two", "someUrl")
-        val collectionsList = mutableListOf<WallpapersCollection>()
-        collectionsList.add(wallpapersCollection)
-        collectionsList.add(wallpapersCollection2)
-        Log.d(TAG, collectionsList.size.toString())
-        emit(collectionsList)
-    }.flowOn(Dispatchers.IO)*/
-
-    fun getCollectionsList(): Flow<List<WallpapersCollection>> = flow<List<WallpapersCollection>> {
-        val collectionsList = mutableListOf<WallpapersCollection>()
-        firestoreReference
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    val collection = WallpapersCollection(
-                        document.getString("title").toString(),
-                        document.getString("imageUrl").toString()
-                    )
-                    collectionsList.add(collection)
-                }
-                Log.d(TAG, "in addOnSuccessListener " + collectionsList.size.toString())
-            }
-            .addOnFailureListener {
-                it.printStackTrace()
-            }
-        Thread.sleep(1000)
-        Log.d(TAG, "before emit " + collectionsList.size.toString())
+    fun getCollectionsList(): Flow<List<WallpapersCollection>> = flow {
+       val collectionsList = suspendCoroutine<List<WallpapersCollection>> { continuation ->
+           firestoreReference
+               .get()
+               .addOnSuccessListener { result ->
+                   val list = mutableListOf<WallpapersCollection>()
+                   for (document in result) {
+                       val collection = WallpapersCollection(
+                           document.getString("title").toString(),
+                           document.getString("imageUrl").toString()
+                       )
+                       list.add(collection)
+                   }
+                   continuation.resume(list)
+               }
+               .addOnFailureListener { exception ->
+                   //TODO handle the exception
+               }
+       }
         emit(collectionsList)
     }.flowOn(Dispatchers.IO)
 
