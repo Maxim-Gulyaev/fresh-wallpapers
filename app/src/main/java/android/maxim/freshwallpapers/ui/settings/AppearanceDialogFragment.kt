9f6d7797e013c4f4bfbce3b6952833447f155c86
MaximGuljaev <maximuspo4ta@gmail.com>
2023-08-01 12:53:17 +0300

    enable ok button in dialog fragment

--- app/src/main/java/android/maxim/freshwallpapers/ui/settings/AppearanceDialogFragment.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/settings/AppearanceDialogFragment.kt
@@ -2,15 +2,17 @@ package android.maxim.freshwallpapers.ui.settings
 
 import android.maxim.freshwallpapers.R
 import android.maxim.freshwallpapers.databinding.FragmentDialogAppearanceBinding
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.fragment.app.DialogFragment
 
-class AppearanceDialogFragment: DialogFragment(R.layout.fragment_dialog_appearance) {
+class AppearanceDialogFragment: DialogFragment(R.layout.fragment_dialog_appearance), OnClickListener {
 
     private var _binding: FragmentDialogAppearanceBinding? = null
     private val binding get() = _binding!!
@@ -22,17 +24,38 @@ class AppearanceDialogFragment: DialogFragment(R.layout.fragment_dialog_appearan
     ): View {
         _binding = FragmentDialogAppearanceBinding.inflate(layoutInflater, container, false)
 
+        /*val currentNightMode = this.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        when (currentNightMode) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                binding.rbLight.isChecked = true
+            }
+            Configuration.UI_MODE_NIGHT_YES -> {
+                binding.rbDark.isChecked = true
+            }
+        }*/
+
         binding.rgAppearanceDialog.setOnCheckedChangeListener { _, checkedId ->
             val clickedButton = view?.findViewById<RadioButton>(checkedId)
             binding.apply {
                 when (clickedButton) {
                     rbLight -> setDefaultNightMode(MODE_NIGHT_NO)
                     rbDark -> setDefaultNightMode(MODE_NIGHT_YES)
-                    rbSystem -> setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
+                    rbSystem ->
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM);
+                        } else {
+                            setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY);
+                        }
                 }
             }
         }
 
+        binding.btnAppearanceDialogOk.setOnClickListener(this)
+
         return binding.root
+    }
+
+    override fun onClick(v: View?) {
+        this.dismiss()
     }
 }
