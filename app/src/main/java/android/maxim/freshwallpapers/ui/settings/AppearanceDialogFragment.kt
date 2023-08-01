2bb7288d5b358f614d4002db570eb7e60736dd3c
MaximGuljaev <maximuspo4ta@gmail.com>
2023-08-01 17:57:45 +0300

    move dark mode change to ok button

--- app/src/main/java/android/maxim/freshwallpapers/ui/settings/AppearanceDialogFragment.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/settings/AppearanceDialogFragment.kt
@@ -1,5 +1,6 @@
 package android.maxim.freshwallpapers.ui.settings
 
+import android.content.SharedPreferences
 import android.maxim.freshwallpapers.R
 import android.maxim.freshwallpapers.app.FreshWallpapersApp
 import android.maxim.freshwallpapers.databinding.FragmentDialogAppearanceBinding
@@ -10,7 +11,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
-import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.fragment.app.DialogFragment
 
@@ -18,6 +18,7 @@ class AppearanceDialogFragment: DialogFragment(R.layout.fragment_dialog_appearan
 
     private var _binding: FragmentDialogAppearanceBinding? = null
     private val binding get() = _binding!!
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,17 +27,7 @@ class AppearanceDialogFragment: DialogFragment(R.layout.fragment_dialog_appearan
     ): View {
         _binding = FragmentDialogAppearanceBinding.inflate(layoutInflater, container, false)
 
-        /*val currentNightMode = this.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-        when (currentNightMode) {
-            Configuration.UI_MODE_NIGHT_NO -> {
-                binding.rbLight.isChecked = true
-            }
-            Configuration.UI_MODE_NIGHT_YES -> {
-                binding.rbDark.isChecked = true
-            }
-        }*/
-
-        val sharedPreferences = FreshWallpapersApp.sharedPreferences
+        sharedPreferences = FreshWallpapersApp.sharedPreferences
 
         when (sharedPreferences.getInt(MODE_KEY, SYSTEM_MODE)) {
             LIGHT_MODE -> binding.rbLight.isChecked = true
@@ -44,35 +35,30 @@ class AppearanceDialogFragment: DialogFragment(R.layout.fragment_dialog_appearan
             SYSTEM_MODE -> binding.rbSystem.isChecked = true
         }
 
-        binding.rgAppearanceDialog.setOnCheckedChangeListener { _, checkedId ->
-            val clickedButton = view?.findViewById<RadioButton>(checkedId)
-            binding.apply {
-                when (clickedButton) {
-                    rbLight -> {
-                        setDefaultNightMode(MODE_NIGHT_NO)
-                        sharedPreferences.edit().putInt(MODE_KEY, LIGHT_MODE).apply()
-                    }
-                    rbDark -> {
-                        setDefaultNightMode(MODE_NIGHT_YES)
-                        sharedPreferences.edit().putInt(MODE_KEY, DARK_MODE).apply()
-                    }
-                    rbSystem -> {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                            setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM);
-                        } else {
-                            setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY);
-                        }
-                        sharedPreferences.edit().putInt(MODE_KEY, SYSTEM_MODE).apply()
-                    }
-                }
-            }
-        }
         binding.btnAppearanceDialogOk.setOnClickListener(this)
 
         return binding.root
     }
 
     override fun onClick(v: View?) {
+        when {
+            binding.rbLight.isChecked -> {
+                setDefaultNightMode(MODE_NIGHT_NO)
+                sharedPreferences.edit().putInt(MODE_KEY, LIGHT_MODE).apply()
+            }
+            binding.rbDark.isChecked -> {
+                setDefaultNightMode(MODE_NIGHT_YES)
+                sharedPreferences.edit().putInt(MODE_KEY, DARK_MODE).apply()
+            }
+            binding.rbSystem.isChecked -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM);
+                } else {
+                    setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY);
+                }
+                sharedPreferences.edit().putInt(MODE_KEY, SYSTEM_MODE).apply()
+            }
+        }
         this.dismiss()
     }
 }
