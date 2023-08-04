542f22d0e8697b70b47da708afec8ee12afd169f
MaximGuljaev <maximuspo4ta@gmail.com>
2023-08-04 18:09:15 +0300

    inject prefs in main activity

--- app/src/main/java/android/maxim/freshwallpapers/MainActivity.kt
+++ app/src/main/java/android/maxim/freshwallpapers/MainActivity.kt
@@ -1,29 +1,31 @@
 package android.maxim.freshwallpapers
 
-import android.maxim.freshwallpapers.app.FreshWallpapersApp
+import android.content.SharedPreferences
 import android.maxim.freshwallpapers.utils.DARK_MODE
 import android.maxim.freshwallpapers.utils.LIGHT_MODE
 import android.maxim.freshwallpapers.utils.MODE_KEY
 import android.maxim.freshwallpapers.utils.SYSTEM_MODE
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate.*
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.appcompat.app.AppCompatDelegate.*
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
     private lateinit var analytics: FirebaseAnalytics
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sharedPreferences = FreshWallpapersApp.sharedPreferences
         val darkMode = sharedPreferences.getInt(MODE_KEY, SYSTEM_MODE)
         when (darkMode) {
             LIGHT_MODE -> setDefaultNightMode(MODE_NIGHT_NO)
--- app/src/main/java/android/maxim/freshwallpapers/app/FreshWallpapersApp.kt
+++ app/src/main/java/android/maxim/freshwallpapers/app/FreshWallpapersApp.kt
@@ -1,19 +1,7 @@
 package android.maxim.freshwallpapers.app
 
 import android.app.Application
-import android.content.SharedPreferences
-import android.maxim.freshwallpapers.utils.DARK_MODE_SHARE_PREFS
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FreshWallpapersApp: Application() {
-
-    companion object {
-        lateinit var sharedPreferences: SharedPreferences
-    }
-
-    override fun onCreate() {
-        sharedPreferences = getSharedPreferences(DARK_MODE_SHARE_PREFS, MODE_PRIVATE)
-        super.onCreate()
-    }
-}
+class FreshWallpapersApp: Application()
