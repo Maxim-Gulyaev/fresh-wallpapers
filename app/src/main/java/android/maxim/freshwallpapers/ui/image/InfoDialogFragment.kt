d962f8862d5575835081ff0591c0f90411b8f511
MaximGuljaev <maximuspo4ta@gmail.com>
2023-08-25 13:25:54 +0300

    rename info fragment

--- app/src/main/java/android/maxim/freshwallpapers/ui/image/InfoDialogFragment.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/image/InfoDialogFragment.kt
@@ -3,5 +3,5 @@ package android.maxim.freshwallpapers.ui.image
 import android.maxim.freshwallpapers.R
 import androidx.fragment.app.DialogFragment
 
-class InfoFragment: DialogFragment(R.layout.fragment_info) {
+class InfoDialogFragment: DialogFragment(R.layout.fragment_info) {
 }
