a3c00c1f0dbb7030c3076815fbd954a36a0b2b3e
MaximGuljaev <maximuspo4ta@gmail.com>
2023-08-25 13:34:45 +0300

    make visible rounded corners

--- app/src/main/java/android/maxim/freshwallpapers/ui/image/InfoDialogFragment.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/image/InfoDialogFragment.kt
@@ -1,7 +1,25 @@
 package android.maxim.freshwallpapers.ui.image
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.maxim.freshwallpapers.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 
 class InfoDialogFragment: DialogFragment(R.layout.fragment_info) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        //transparent background to make visible rounded corners
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
