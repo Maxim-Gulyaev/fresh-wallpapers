8cadb57f61da9fcf0ab0f62be911d832afe4c2d3
MaximGuljaev <maximuspo4ta@gmail.com>
2023-08-27 15:42:18 +0300

    make rounded corners in apply dialog

--- app/src/main/java/android/maxim/freshwallpapers/ui/image/ApplyDialogFragment.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/image/ApplyDialogFragment.kt
@@ -1,7 +1,25 @@
 package android.maxim.freshwallpapers.ui.image
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.maxim.freshwallpapers.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 
 class ApplyDialogFragment: DialogFragment(R.layout.fragment_dialog_apply) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        //transparent background to make visible rounded corners
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
