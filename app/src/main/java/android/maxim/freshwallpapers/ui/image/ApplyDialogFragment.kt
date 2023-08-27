cbf1a8e20ddfbfb2474b73affae77ea652914b79
MaximGuljaev <maximuspo4ta@gmail.com>
2023-08-27 21:37:26 +0300

    add binding and implement cancel button in apply fragment

--- app/src/main/java/android/maxim/freshwallpapers/ui/image/ApplyDialogFragment.kt
+++ app/src/main/java/android/maxim/freshwallpapers/ui/image/ApplyDialogFragment.kt
@@ -3,6 +3,7 @@ package android.maxim.freshwallpapers.ui.image
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.maxim.freshwallpapers.R
+import android.maxim.freshwallpapers.databinding.FragmentDialogApplyBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,15 +12,23 @@ import androidx.fragment.app.DialogFragment
 
 class ApplyDialogFragment: DialogFragment(R.layout.fragment_dialog_apply) {
 
+    private var _binding: FragmentDialogApplyBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentDialogApplyBinding.inflate(layoutInflater, container, false)
 
         //transparent background to make visible rounded corners
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding.btnApplyDialogCancel.setOnClickListener {
+            this.dismiss()
+        }
+
+        return binding.root
     }
 }
