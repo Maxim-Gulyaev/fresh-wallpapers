d6c72f9bfd5c97e416b0b9e4076c54a3088772ae
MaximGuljaev <maximuspo4ta@gmail.com>
2023-09-01 16:44:31 +0300

    add no results fragment

---
+++ app/src/main/java/android/maxim/freshwallpapers/ui/imagelist/NoResultsFragment.kt
@@ -0,0 +1,7 @@
+package android.maxim.freshwallpapers.ui.imagelist
+
+import android.maxim.freshwallpapers.R
+import androidx.fragment.app.Fragment
+
+class NoResultsFragment: Fragment(R.layout.fragment_no_results) {
+}
