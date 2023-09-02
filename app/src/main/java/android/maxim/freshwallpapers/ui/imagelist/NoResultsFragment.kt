a3e15e584574fe96396617224d3d95f1329d0de4
MaximGuljaev <maximuspo4ta@gmail.com>
2023-09-02 17:07:07 +0300

    delete no results dialog fragment

--- app/src/main/java/android/maxim/freshwallpapers/ui/imagelist/NoResultsFragment.kt
+++
@@ -1,7 +0,0 @@
-package android.maxim.freshwallpapers.ui.imagelist
-
-import android.maxim.freshwallpapers.R
-import androidx.fragment.app.Fragment
-
-class NoResultsFragment: Fragment(R.layout.fragment_no_results) {
-}
